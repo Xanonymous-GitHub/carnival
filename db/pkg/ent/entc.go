832346407a4e12565ec381203b1d66bca518f156
Xanonymous <trusaidlin@gmail.com>
2022-02-15 12:15:08 +0800

    docs: add openapi json

--- db/pkg/ent/entc.go
+++ db/pkg/ent/entc.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"entgo.io/contrib/entoas"
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
 	"github.com/hedwigz/entviz"
@@ -10,7 +11,18 @@ import (
 )
 
 func main() {
+	// generate db diagram
 	err := entc.Generate("./schema", &gen.Config{}, entc.Extensions(entviz.Extension{}))
+	if err != nil {
+		log.Fatalf("running ent codegen: %v", err)
+	}
+
+	// generate OpenApi spec
+	ex, err := entoas.NewExtension()
+	if err != nil {
+		log.Fatalf("creating entoas extension: %v", err)
+	}
+	err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(ex))
 	if err != nil {
 		log.Fatalf("running ent codegen: %v", err)
 	}
